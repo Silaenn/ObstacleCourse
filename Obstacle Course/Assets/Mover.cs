f1d534c7c56e15fa42a6748bef22ab567faab542
Deo silaen <deokeldisilaen@gmail.com>
2025-01-24 14:17:39 +0700

    addPlayer

---
+++ Obstacle Course/Assets/Mover.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float speed = 2f;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        float xValue = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        float zValue = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+
+        transform.Translate(xValue, 0, zValue);
+    }
+}
